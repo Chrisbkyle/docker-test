9f63bcf20d3fba09c052e5a68c12c6d5a5d18910
Chris <christopher.b.kyle@gmail.com>
2023-07-06 23:48:34 +1200

    whitelist

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);    
 
 // const whitelist = ['http://localhost:3000', 'http://3.25.114.242/'];
-const whitelist = ['http://3.25.114.242:3000'];
+const whitelist = ['http://3.25.114.242//'];
 const corsOptions = {
     origin: function (origin, callback) {
         if(!origin || whitelist.indexOf(origin) !== -1) {
